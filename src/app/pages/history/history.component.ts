d36c7910c661e6d4851f349eac63e02bdfd2e0c3
Jean Marques <jean.magic@hotmail.com>
2025-03-18 22:02:27 -0300

    feat: improvements style in history pages and adjust necessary

--- src/app/pages/history/history.component.ts
+++ src/app/pages/history/history.component.ts
@@ -3,20 +3,29 @@ import { CommonModule } from '@angular/common';
 import { HistoryService } from '../../services/history.service';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { HistoryViewComponent } from '../history-view/history-view.component';
 
 @Component({
   selector: 'app-history',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, HistoryViewComponent],
   templateUrl: './history.component.html',
   styleUrl: './history.component.css'
 })
+
 export class HistoryComponent implements OnInit {
   searchQuery = signal('');
   historyList = signal<any[]>([]);
   currentPage = signal(1);
   totalPages = signal(1);
   isLoading = signal(false);
+
+  // deleteModalOpen = false;
+  // historyToDelete: string | null = null;
+
+  showHistoryTable = true;
+  selectedHistory: any = null;
+  showViewModal = false;
 
   constructor(private HistoryService: HistoryService, private router: Router) {}
 
@@ -26,9 +35,8 @@ export class HistoryComponent implements OnInit {
 
   fetchUserHistory() {
     this.isLoading.set(true);
-    this.HistoryService.getUserHistory(this.searchQuery(), this.currentPage()).subscribe({
+    this.HistoryService.getHistory(this.searchQuery(), this.currentPage()).subscribe({
       next: (data) => {
-        // console.log('Data received:', data.data);
         this.historyList.update(() => data.data);
         this.totalPages.update(() => data.last_page);
         this.isLoading.set(false);
@@ -58,15 +66,47 @@ export class HistoryComponent implements OnInit {
       return Array.from({ length: this.totalPages() }, (_, i) => i + 1);
   }
 
-  // viewRecord(id: number) {
-  //   this.router.navigate(['/history', id, 'view']);
+  createHistory() {
+    this.router.navigate(['/history/create']);
+  }
+  editHistory(id: string) {
+    this.router.navigate([`/history/edit/${id}`]);
+  }
+
+  viewHistory(id: number): void {
+    this.HistoryService.getById(id).subscribe((data) => {
+        this.selectedHistory = data;
+        this.showViewModal = true;
+        this.showHistoryTable = false;
+    });
+  }
+
+  closeViewHistory(): void {
+    this.showViewModal = false;
+    this.selectedHistory = null;
+    this.showHistoryTable = true;
+  }
+
+  // openDeleteModal(id: string) {
+  //   this.historyToDelete = id;
+  //   this.deleteModalOpen = true;
   // }
 
-  // editRecord(id: number) {
-  //   this.router.navigate(['/history', id, 'edit']);
+  // closeDeleteModal() {
+  //   this.historyToDelete = null;
+  //   this.deleteModalOpen = false;
+  // }
+
+  // deleteHistory() {
+  //   if (this.historyToDelete) {
+  //     this.HistoryService.delete(this.historyToDelete).subscribe(() => {
+  //       this.fetchUserHistory();
+  //       this.closeDeleteModal();
+  //     });
+  //   }
   // }
 
   getSpecialtiesNames(record: any): string {
     return record.specialties?.map((s: any) => s.name).join(', ') || 'Sem especialidades';
-  }  
+  }
 }
